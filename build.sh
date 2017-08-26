51078c8b5c3b615187f4c7f60fbe7bc357930dd3
gaulouis <gaulouis.fr@gmail.com>
2017-08-26 13:00:23 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+antlr=/home/gaulouis/local/src/glib-antlr4.7/tool/target/antlr4-4.7-complete.jar
+export CLASSPATH=".:$antlr:$CLASSPATH"
+
+
+ENABLE=1
+
+if [ "$ENABLE" -eq 1 ]; then
+
+echo "Generate TestLexer"
+java -Xmx500M -cp "$antlr:$CLASSPATH" org.antlr.v4.Tool PhpLexer.g
+echo "Generate TestParser"
+java -Xmx500M -cp "$antlr:$CLASSPATH" org.antlr.v4.Tool PhpParser.g
+echo "Compiling ..."
+javac Test*.java
+
+fi
+
+echo 'parsing "<?php echo ?>"'
+echo "<?php echo ?>" | java org.antlr.v4.gui.TestRig Test block -tree
+
+echo 'parsing "<?php echo Hell ?>"'
+echo "<?php echo Hell ?>" | java org.antlr.v4.gui.TestRig Test block -tree
+
+
